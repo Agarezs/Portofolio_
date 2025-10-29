bb3a0bffc2bbb53089445d0c911052d5af6576c5
enggar <enggarraharjo86@gmail.com>
2025-10-30 00:05:24 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -77,7 +77,7 @@ const SocialLink = memo(({ icon: Icon, link }) => (
 const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
-const WORDS = ["Network & Telecom Student", "Tech Enthusiast", "Gaming"];
+const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind", "Python"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/Agarezs" },
@@ -242,5 +242,6 @@ const Home = () => {
     </div>
   );
 };
+
 
 export default memo(Home);
